e4ad029d80a95c5665b8463524f91cc8078cc1a9
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-07-06 11:03:53 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+namespace sprites {
+     /**
+      *  make the right gravity
+      */
+    //% block
+    //% group="Advanced"
+    export function makeNormalGravity(): number {
+        
+        
+        return 400
+    }
+    /**
+     * use how many amount of gravity 
+     * e.g. 400.5
+     */
+    //% block
+    export function makeGravityTo(gravity: number): number {
+        return gravity
+    }
+}
